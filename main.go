fd9927872fdf7f8f5464bbda2ef28ce01272f98e
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-03-14 16:02:37 +0300

    Clean up main.

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/LavyshAlexander/data-structures/list/LinkedList"
-)
-
 func main() {
-	list := LinkedList.LinkedList{}
-	list.Append(10)
-	fmt.Println(list.String())
 }
