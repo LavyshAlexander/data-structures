edbe42fcc7ef78d7023968992fd2802a495450ab
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-03-17 09:40:31 +0300

    Rewrite List type with generics.

--- list/types.go
+++ list/types.go
@@ -1,9 +1,9 @@
 package list
 
-type List interface {
-	Append(value int)
-	Prepend(value int)
-	Delete(value int) bool
-	Exist(value int) bool
+type List[T any] interface {
+	Append(value T)
+	Prepend(value T)
+	Delete(value T) bool
+	Exist(value T) bool
 	Length() int
 }
