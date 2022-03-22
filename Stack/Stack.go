21729fc476cc21fae911a68314e56d273fd4006e
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-03-22 15:31:10 +0300

    Refactor Stack implementation without usage of LinkedList.

--- Stack/Stack.go
+++ Stack/Stack.go
@@ -1,27 +1,28 @@
 package Stack
 
-import "github.com/LavyshAlexander/data-structures/LinkedList"
+import "fmt"
 
-type Stack[T comparable] struct {
-	list *LinkedList.LinkedList[T]
-}
-
-func (s *Stack[T]) Push(value T) {
-	s.list.Prepend(value)
-}
-
-func (s *Stack[T]) Pop() (ret T) {
-	popped := s.list.Head
-	if popped != nil {
-		s.list.Head = popped.Next
-		ret = popped.Value
+type (
+	Stack[T any] struct {
+		top    *Node[T]
+		length int
 	}
+	Node[T any] struct {
+		value    T
+		previous *Node[T]
+	}
+)
 
-	return
+func New[T comparable]() *Stack[T] {
+	return &Stack[T]{nil, 0}
+}
+
+func (s *Stack[T]) Length() int {
+	return s.length
 }
 
 func (s *Stack[T]) IsEmpty() bool {
-	return s.list.Head == nil
+	return s.top == nil
 }
 
 func (s *Stack[T]) Peek() T {
@@ -29,9 +30,38 @@ func (s *Stack[T]) Peek() T {
 		return *new(T)
 	}
 
-	return s.list.Head.Value
+	return s.top.value
+}
+
+func (s *Stack[T]) Push(value T) {
+	top := &Node[T]{value, s.top}
+	s.top = top
+}
+
+func (s *Stack[T]) Pop() (ret T) {
+	popped := s.top
+
+	if popped != nil {
+		s.top = popped.previous
+		return popped.value
+	}
+
+	return
 }
 
 func (s *Stack[T]) String() string {
-	return s.list.String()
+	if s.top == nil {
+		return "[]"
+	}
+
+	node := s.top
+
+	str := "[ "
+	for node != nil {
+		str += fmt.Sprintf("%v ", node.value)
+		node = node.previous
+	}
+	str += "]"
+
+	return str
 }
--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import "github.com/LavyshAlexander/data-structures/LinkedList"
-
 func main() {
-	l := LinkedList.LinkedList[int]{}
-	l.Append(10)
-	l.Append(20)
-	l.Append(30)
-
-	l.PrintReverse()
 }
+
